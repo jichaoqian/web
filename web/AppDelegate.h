ec8536b2954fe8c2628399c4ff5740e56127fd66
jichaoqian <jichaoqian@iotogether.com>
2018-06-20 19:22:25 +0800

    sss

---
+++ web/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  web
+//
+//  Created by TUOGE on 2018/6/19.
+//  Copyright © 2018年 iotogether. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ web/ViewController.m
@@ -0,0 +1,107 @@
+//
+//  ViewController.m
+//  web
+//
+//  Created by TUOGE on 2018/6/19.
+//  Copyright © 2018年 iotogether. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <WebKit/WebKit.h>
+
+@interface ViewController () <UIWebViewDelegate, WKUIDelegate, WKNavigationDelegate>
+
+@property (nonatomic, strong) WKWebView *webView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    _webView = [[WKWebView alloc] initWithFrame:self.view.frame];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"https://www.jianshu.com/u/348a2b6efe3d"]];
+    _webView.UIDelegate          = self;
+    _webView.navigationDelegate  = self;
+    [_webView loadRequest:request];
+    [self.view addSubview:_webView];
+}
+
+// WKNavigationDelegate
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
+    NSLog(@"是否允许这个导航");
+    decisionHandler(WKNavigationActionPolicyAllow);
+}
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler {
+    NSLog(@"%@", navigationResponse);
+    NSLog(@"知道返回的内容之后，是否允许加载");
+    // 允许加载
+    decisionHandler(WKNavigationResponsePolicyAllow);
+}
+
+// 开始加载
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
+    NSLog(@"开始加载");
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+}
+
+
+- (void)webView:(WKWebView *)webView didReceiveServerRedirectForProvisionalNavigation:(WKNavigation *)navigation {
+    NSLog(@"接收到服务器跳转请求之后调用");
+}
+
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation withError:(nonnull NSError *)error {
+    NSLog(@"网页加载失败");
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+}
+
+- (void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation {
+    NSLog(@"开始接收网页内容");
+}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+    NSLog(@"网页导航加载完毕");
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    self.title = webView.title;
+    [webView evaluateJavaScript:@"document.title" completionHandler:^(id _Nullable tempTitle, NSError * _Nullable error) {
+        NSLog(@"document.title:%@>>>webView title:%@", tempTitle, webView.title);
+    }];
+}
+
+- (void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error {
+    NSLog(@"加载失败原因：%@", [error description]);
+}
+
+- (void)webViewWebContentProcessDidTerminate:(WKWebView *)webView {
+    NSLog(@"网页加载进程终止");
+}
+
+- (void)webView:(WKWebView *)webView didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition, NSURLCredential * _Nullable))completionHandler {
+    NSLog(@"收到");
+    // 加载https 权限认证代理方法
+    if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]) {
+        if ([challenge previousFailureCount] == 0) {
+            NSURLCredential *credential = [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust];
+            completionHandler(NSURLSessionAuthChallengeUseCredential, credential);
+        } else {
+            completionHandler(NSURLSessionAuthChallengeCancelAuthenticationChallenge, nil);
+        }
+    } else {
+        completionHandler(NSURLSessionAuthChallengeCancelAuthenticationChallenge, nil);
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc {
+//    [SVProgressHUD dismiss];
+}
+
+@end
+
